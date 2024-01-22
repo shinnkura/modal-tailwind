78eda3e5893146c1988685ca44dcf590887ad1ef
shinnkura <n.shinzo1010@icloud.com>
2024-01-22 13:55:36 +0900

    feat(サイドバー): タップ時に左側にマーカーを設定

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ const customStyles = {
     backgroundColor: "#FFFFFF",
     color: "#494C4C",
     borderRadius: "10px",
-
     maxWidth: "1000px",
     height: "590px",
     margin: "auto",
@@ -22,30 +21,39 @@ const customStyles = {
 
 export default function Home() {
   const [modalOpen, setModalOpen] = useState(false);
-  const [isActive, setIsActive] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  // const [isActive, setIsActive] = useState(false);
 
-  // モーダルを開くための関数
   function openModal() {
     setModalOpen(true);
   }
 
-  // モーダルを閉じるための関数
   function closeModal() {
     setModalOpen(false);
   }
 
-  // モーダルの中身を切り替えるための関数
-  const handleClick = () => {
-    setIsActive(!isActive);
+  const handleClick = (item: string) => {
+    setSelectedItem(item);
+  };
+
+  const isActive = (item: string) => {
+    return selectedItem === item;
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={openModal} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <button
+        onClick={openModal}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
         Open Modal
       </button>
 
-      <Modal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)} style={customStyles}>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+        style={customStyles}
+      >
         <div className="flex items-center justify-between px-7 py-3 text-custom-blue font-bold">
           <h2>施設形態</h2>
           <CloseIcon onClick={closeModal} />
@@ -53,25 +61,74 @@ export default function Home() {
         <div className="flex">
           <div className="w-1/3 border-r-2">
             <h3
-              className={`pt-2.5 pb-2.5 pl-7.5  ${
-                isActive ? "text-custom-red font-bold after:border-l-2 afeter:border-custom-red" : ""
-              } 	`}
-              onClick={handleClick}
+              className={`pt-2.5 pb-2.5 pl-7.5 relative ${
+                isActive("facility")
+                  ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
+                  : ""
+              }`}
+              onClick={() => handleClick("facility")}
             >
               施設形態を選択
             </h3>
+            <h3 className="pt-2.5 pb-2.5 pl-7.5 relative">勤務地を選択</h3>
             <ul className="pt-2.5 pb-2.5 pl-7.5">
-              <li className="pt-2.5 pb-2.5 pl-7.5">東京都</li>
-              <li className="pt-2.5 pb-2.5 pl-7.5">神奈川県</li>
-              <li className="pt-2.5 pb-2.5 pl-7.5">千葉県</li>
-              <li className="pt-2.5 pb-2.5 pl-7.5">埼玉県</li>
+              <li
+                className={`pt-2.5 pb-2.5 pl-7.5 relative ${
+                  isActive("tokyo")
+                    ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
+                    : ""
+                }`}
+                onClick={() => handleClick("tokyo")}
+              >
+                東京都
+              </li>
+              <li
+                className={`pt-2.5 pb-2.5 pl-7.5 relative ${
+                  isActive("kanagawa")
+                    ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
+                    : ""
+                }`}
+                onClick={() => handleClick("kanagawa")}
+              >
+                神奈川県
+              </li>
+              <li
+                className={`pt-2.5 pb-2.5 pl-7.5 relative ${
+                  isActive("chiba")
+                    ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
+                    : ""
+                }`}
+                onClick={() => handleClick("chiba")}
+              >
+                千葉県
+              </li>
+              <li
+                className={`pt-2.5 pb-2.5 pl-7.5 relative ${
+                  isActive("saitama")
+                    ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
+                    : ""
+                }`}
+                onClick={() => handleClick("saitama")}
+              >
+                埼玉県
+              </li>
             </ul>
-            <h3 className="pt-2.5 pb-2.5 pl-7.5">働き方を選択</h3>
+            <h3
+              className={`pt-2.5 pb-2.5 pl-7.5 relative ${
+                isActive("workstyle")
+                  ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
+                  : ""
+              }`}
+              onClick={() => handleClick("workstyle")}
+            >
+              働き方を選択
+            </h3>
           </div>
           <div className="p-3 w-2/3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod cumque commodi quis, sapiente
-            mollitia. Earum dolor veritatis iste dicta? Mollitia vel ullam adipisci officia fuga dolores eveniet quae
-            placeat.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            quod cumque commodi quis, sapiente mollitia. Earum dolor veritatis
+            iste dicta? Mollitia vel ullam adipisci officia fuga dolores eveniet
+            quae placeat.
           </div>
         </div>
         <button
