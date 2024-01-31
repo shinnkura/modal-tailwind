1aa92525e211b61b13bb2e687bfe3ce3da4013a9
shinnkura <n.shinzo1010@icloud.com>
2024-01-31 18:58:58 +0900

    refactor(sidebar): tailwindをリファクタリング

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -12,27 +12,22 @@ const Sidebar = ({ isActive, onClick }: SidebarProps) => {
   return (
     <div className="w-1/3 border-r-2 h-[400px]">
       <h3
-        className={`pt-2.5 pb-2.5 pl-7.5 relative cursor-pointer ${
-          isActive("facility")
-            ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
-            : "hover:bg-[#f3f3f3] hover:after:content-[''] hover:after:block hover:after:border-r-4 hover:after:border-custom-red hover:after:absolute hover:after:right-0 hover:after:top-0 hover:after:bottom-0"
-        } item`}
+        className={`sidebar-item ${isActive("facility") ? "sidebar-item-active" : "sidebar-item-hover"} item`}
         onClick={() => onClick("facility")}
       >
         施設形態を選択
       </h3>
-      <h3 className={`pt-2.5 pb-2.5 pl-7.5 relative cursor-pointer ${isActive("location") ? "text-custom-red " : ""}`}>
+      <h3
+        className={`sidebar-item ${isActive("location") ? "text-custom-red" : "sidebar-item-hover"} item`}
+        onClick={() => onClick("tokyo")}
+      >
         勤務地を選択
       </h3>
       <ul className="pt-2.5 pb-2.5 pl-7.5">
         {regions.map((region) => (
           <li
             key={region}
-            className={`pt-2.5 pb-2.5 relative cursor-pointer ${
-              isActive(region)
-                ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
-                : "hover:bg-[#f3f3f3] hover:after:content-[''] hover:after:block hover:after:border-r-4 hover:after:border-custom-red hover:after:absolute hover:after:right-0 hover:after:top-0 hover:after:bottom-0"
-            } item`}
+            className={`sidebar-item ${isActive(region) ? "sidebar-item-active" : "sidebar-item-hover"} item`}
             onClick={() => onClick(region)}
           >
             <SubdirectoryArrowRightIcon className="pb-2" />
@@ -47,11 +42,7 @@ const Sidebar = ({ isActive, onClick }: SidebarProps) => {
         ))}
       </ul>
       <h3
-        className={`pt-2.5 pb-2.5 pl-7.5 relative cursor-pointer ${
-          isActive("workstyle")
-            ? "text-custom-red font-bold after:content-[''] after:block after:border-r-4 after:border-custom-red after:absolute after:right-0 after:top-0 after:bottom-0"
-            : "hover:bg-[#f3f3f3] hover:after:content-[''] hover:after:block hover:after:border-r-4 hover:after:border-custom-red hover:after:absolute hover:after:right-0 hover:after:top-0 hover:after:bottom-0"
-        } item`}
+        className={`sidebar-item ${isActive("workstyle") ? "sidebar-item-active" : "sidebar-item-hover"} item`}
         onClick={() => onClick("workstyle")}
       >
         働き方を選択
